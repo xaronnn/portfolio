0a7c51ed64f1807cb67e7284b43660d8bf1d49f2
Dennis Kievits <denkievits@gmail.com>
2018-11-03 00:35:32 +0100

    Updates

---
+++ precache-manifest.4fd1fafdb4fc5270b2b3531a985956df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "afdaf599d29cf4cea1a9",
+    "url": "/portfolio/static/js/main.afdaf599.chunk.js"
+  },
+  {
+    "revision": "96981ed661033911d40b",
+    "url": "/portfolio/static/js/1.96981ed6.chunk.js"
+  },
+  {
+    "revision": "afdaf599d29cf4cea1a9",
+    "url": "/portfolio/static/css/main.0ff69c4d.chunk.css"
+  },
+  {
+    "revision": "85190daf2dedf339bc87b473e8bc266c",
+    "url": "/portfolio/index.html"
+  }
+];
