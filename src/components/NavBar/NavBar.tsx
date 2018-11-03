94a5795ccf62c4f5c446dd167491073a1bdaa37f
Dennis Kievits <denkievits@gmail.com>
2018-11-03 18:08:56 +0100

    Mobile nav menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import classNames from 'classnames';
+import MediaQuery from 'react-responsive';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
 const classnames = require('./NavBar.scss');
+const classnamesMobile = require('./NavBarMobile.scss');
 
 interface IProps {
+}
+
+interface IState {
+  open: boolean;
 }
 
 interface INavItem {
@@ -16,25 +25,65 @@ const navItems: INavItem[] = [
   {content: 'Contact'}
 ];
 
-class NavBar extends React.Component<IProps> {
+const switchWidth = 768;
+
+class NavBar extends React.Component<IProps, IState> {
+  public state = {
+    open: false
+  };
+
   public render() {
     return (
-      <nav className={classnames.root}>
-        <ul className={classnames.navList}>
-          {navItems.map((navItem, i) => {
-            const classname = classNames(
-              classnames.navItem,
-              { [classnames.navItemSelected]: navItem.content === 'Home' }
-            );
+      <>
+        <MediaQuery query={`(min-width: ${switchWidth}px)`}>
+          <nav className={classnames.root}>
+            <ul className={classnames.navList}>
+              {navItems.map((navItem, i) => {
+                const classname = classNames(
+                  classnames.navItem,
+                  {[classnames.navItemSelected]: navItem.content === 'Home'}
+                );
 
-            return (
-              <li key={i} className={classname}>{navItem.content}</li>
-            );
-          })}
-        </ul>
-      </nav>
+                return (
+                  <li key={i} className={classname}>{navItem.content}</li>
+                );
+              })}
+            </ul>
+          </nav>
+        </MediaQuery>
+        <MediaQuery query={`(max-width: ${switchWidth}px)`}>
+          <nav className={classnamesMobile.root}>
+            <div
+              className={classnamesMobile.menuIcon}
+              onClick={this.handleIconClick}
+            >
+              <FontAwesomeIcon icon={faBars} size="2x"/>
+            </div>
+          </nav>
+          <div className={classNames(classnamesMobile.menuContainer, {
+            [classnamesMobile.menuContainerHidden]: !this.state.open
+          })}>
+            <ul className={classnamesMobile.navList}>
+              {navItems.map((navItem, i) => {
+                const classname = classNames(
+                  classnamesMobile.navItem,
+                  {[classnamesMobile.navItemSelected]: navItem.content === 'Home'}
+                );
+
+                return (
+                  <li key={i} className={classname}>{navItem.content}</li>
+                );
+              })}
+            </ul>
+          </div>
+        </MediaQuery>
+      </>
     );
   }
+
+  private handleIconClick = () => {
+    this.setState((prevState: IState) => ({open: !prevState.open}));
+  };
 };
 
 export default NavBar;
